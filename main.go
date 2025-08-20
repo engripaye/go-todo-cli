38932d7dff732dcc316953d10bc35d4800c162dc
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-20 05:15:23 +0100

    Initial commit: Go Todo CLI app

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	Title string
+	Done  bool
+}
+
+var tasks []Task
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go add <task>")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide a task description")
+			return
+		}
+		title := os.Args[2]
+		tasks = append(tasks, Task{Title: title, Done: false})
+		saveTasks()
+		fmt.Println("✅ Task added:", title)
+	}
+}
+
+func saveTasks() {
+	file, _ := os.Create("tasks.json")
+	defer file.Close()
+	json.NewEncoder(file).Encode(tasks)
+}
